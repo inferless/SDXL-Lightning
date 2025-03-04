447f8a797262ca175d8449bed1ecddd21a44b255
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-04 19:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from huggingface_hub import snapshot_download
 import torch
 from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
 from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
-
 from io import BytesIO
 import base64
-import os
 
 class InferlessPythonModel:
     def initialize(self):
@@ -14,6 +15,7 @@ class InferlessPythonModel:
       ckpt = "sdxl_lightning_4step_unet.safetensors" # Use the correct ckpt for your step setting!
 
       # Load model.
+      snapshot_download(repo_id=base,allow_patterns=["*.safetensors"])
       unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
       unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device="cuda"))
       self.pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
